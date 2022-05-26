12878b50fa59d2163fd1be8b0aa70f87d1e97381
Jan David <jd@devxbots.com>
2022-05-26 17:00:31 +0200

    Create event type (#3)
    GitHub enables integrations to subscribe to events that happen on the
    platform. Whenever an event happens, a webhook is sent to the
    integration with the event as its payload.
    
    A new `enum` has been created to represent these events. It has a
    catch-all variant for events that the crate does not support yet, which
    wraps the webhook payload as a JSON value.

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,11 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
-}
+//! Types and actions for GitHub
+//!
+//! [`github-parts`] is a tailor-made integration with GitHub. It is designed to make it easier to
+//! build GitHub Apps, and thus focuses mostly on [webhook events and payloads]. The crate contains
+//! _types_ that represent resources on GitHub, and _actions_ that can create and modify them.
+//!
+//! [webhook events and payloads]: https://docs.github.com/en/developers/webhooks-and-events/webhooks/webhook-events-and-payloads
+
+#![warn(missing_docs)]
+
+pub mod event;
