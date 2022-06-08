95b0ce2a25f2d5b2755833b0622e67d59333403c
Jan David <jd@devxbots.com>
2022-06-08 11:35:13 +0200

    Refactor error types (#21)
    The error types in the crate have been refactored to increase their
    usability. Overly verbose variants of both error types have been
    removed, and the anyhow crate has been added to capture context around
    unexpected errors.
    
    This is just the first step on a long journey to provide a clean API for
    errors that can occur within the crate. As we gain more experience with
    actions and the workflows built on top of them, we will most likely
    refactor the errors at least once.

--- src/action/get_file/error.rs
+++ src/action/get_file/error.rs
@@ -1,42 +1,15 @@
-use base64::DecodeError;
 use thiserror::Error;
-
-use crate::error::Error;
 
 /// Errors that can occur when getting a file from GitHub
 #[derive(Debug, Error)]
 pub enum GetFileError {
-    /// An invalid argument was passed to the action
-    #[error("argument was invalid. {0}")]
-    Argument(String),
-
-    /// Authenticating with GitHub failed
-    #[error("authentication failed. {0}")]
-    Authentication(#[from] Error),
-
-    /// Decoding the file's content failed
-    #[error("decoding content failed. {0}")]
-    Decoding(#[from] DecodeError),
-
-    /// The user tried to get a directory
-    #[error("path was a directory, but must be a file")]
-    Directory,
-
-    /// The file encoding is not supported by the crate
-    #[error("encoding {0} is not supported")]
-    Encoding(String),
-
     /// The file was not found
     #[error("file not found")]
     NotFound,
 
-    /// The outgoing request to GitHub failed
-    #[error("querying the content failed. {0}")]
-    Request(#[from] reqwest::Error),
-
-    /// Deserializing the payload in the API response failed
-    #[error("response could not be deserialized. {0}")]
-    Response(#[from] serde_json::Error),
+    /// The user tried to get a directory
+    #[error("path was a directory, but must be a file")]
+    Directory,
 
     /// The user tried to get a git submodule
     #[error("path was a submodule, but must be a file")]
@@ -46,6 +19,10 @@ pub enum GetFileError {
     // TODO: Follow symlinks and return the file
     #[error("path was a symlink, but must be a file")]
     Symlink,
+
+    /// An unexpected error occurred while running the action
+    #[error(transparent)]
+    UnexpectedError(#[from] anyhow::Error),
 }
 
 #[cfg(test)]
