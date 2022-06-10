06eabb8cfe8967bf0b7ad7f5c8da5fa7ee616e70
Jan David <jd@devxbots.com>
2022-06-10 13:16:39 +0200

    Create trait for actions (#31)
    Actions all follow the same pattern: They accept an input, interact with
    GitHub, and then return a result. As we are creating more actions, we
    want to capture as much shared logic as possible and make it reusable.
    For this purpose, a new trait has been created that describes an action.
    
    The action to list check runs has been refactored already. The action to
    get a file however has a slightly different internal logic that is not
    (yet) compatible with the trait.

--- src/action/mod.rs
+++ src/action/mod.rs
@@ -1,2 +1,16 @@
+use async_trait::async_trait;
+use serde::de::DeserializeOwned;
+use serde::Serialize;
+
 pub mod get_file;
 pub mod list_check_runs;
+
+#[async_trait]
+pub trait Action<Input, Output, Error>
+where
+    Input: Serialize,
+    Output: DeserializeOwned,
+    Error: std::error::Error,
+{
+    async fn execute(&self, input: &Input) -> Result<Output, Error>;
+}
